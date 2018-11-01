42ed737d2a28ab88b6a6828a8a4ac142f639b06f
VasilDachev <vasildachevnew@gmail.com>
2018-11-01 10:35:02 +0200

    Solved Chap1

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете програма, която извежда текущите дата и час.
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете програма, която смята корен квадратен от 12345.
+            double Square = 12345;
+            Console.WriteLine(System.Math.Sqrt(Square));
+        }
+    }
+}
